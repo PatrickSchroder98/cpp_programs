2b62df6be3b766ac2b470e9cabdfaddb0c8154f2
Patrick Schröder <patrickschroder479@gmail.com>
2023-11-17 13:04:39 +0100

    Updated income.cpp

--- income.cpp
+++ income.cpp
@@ -9,9 +9,18 @@ void yearly_income(float n) {
 }
 
 
+void hourly_income(float n) {
+    // converter hourly income to monthly and yearly
+    std::cout << "Hourly income: " << n << std::endl;
+    std::cout << "Monthly income: " << ((n * 40) * 4) << std::endl;
+    std::cout << "Yearly income: " << ((n * 40) * 4) * 12 << std::endl;
+
+}
+
 int main()
 {
     yearly_income(150000);
+    hourly_income(78.12);
 
     return 0;
 }
