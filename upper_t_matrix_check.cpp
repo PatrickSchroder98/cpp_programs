cd7ed39d0965efcf983dd818c1d17a99d0440dd3
Patrick Schröder <patrickschroder479@gmail.com>
2023-12-06 12:26:47 +0100

    Updated upper_t_matrix_check.cpp

--- upper_t_matrix_check.cpp
+++ upper_t_matrix_check.cpp
@@ -2,25 +2,33 @@
 #include <vector>
 
 
-bool upper_triangular_matrix_check(std::vector<std::vector<int>> vect) {
-    // returns true if given matrix is an upper triangular matrix
+bool upper_lower_triangular_matrix_check(std::vector<std::vector<int>> vect, bool lower) {
+    // returns true if given matrix is an upper or lower triangular matrix - depending on boolean argument
     
     for (int i = 0; i < vect.size(); i++)
     {
         for (int j = 0; j < vect[i].size(); j++)
         {
-            if (i <= j) {
-                if (vect[i][j] == 0) {
-                    return false;
+            if (lower) {
+                if (i < j) { // lower triangular matrix check
+
+                    if (vect[i][j] != 0) {
+                        return false;
+                    }
                 }
             }
-            else
-            { 
-                if (vect[i][j] != 0) {
-                    return false;
+            else {
+                if (i > j) { // upper triangular matrix check
+
+                    if (vect[i][j] != 0) {
+                        return false;
+                    }
                 }
             }
+            
+
         }
+        
     }
     return true;
 }
@@ -32,12 +40,12 @@ int main() {
 
     std::vector<std::vector<int>> vect
     {
-       {1, 1, 1},
-       {0, 1, 1},
+       {1, 0, 1},
+       {0, 1, 0},
        {0, 0, 1}
     };
     
-    std::cout << upper_triangular_matrix_check(vect) << " ";
+    std::cout << upper_lower_triangular_matrix_check(vect, false) << " ";
 
     return 0;
 }
